44357b81bbea193a0c8c0c79b51b14fa2109010f
Pieter Belmans <pieterbelmans@gmail.com>
2014-11-28 07:11:41 +0100

    Initial commit
    Truly basic note and interval manipulation code. It's good to know Python has operator overloading!

---
+++ counterpoint.py
@@ -0,0 +1,100 @@
+class Interval:
+  _value = 0
+
+  def __init__(self, value):
+    assert abs(value) <= 127
+
+    self._value = value
+
+  # operations
+  def __add__(self, other):
+    return Interval(self._value + other._value)
+
+  def __sub__(self, other):
+    return Interval(self._value - other._value)
+
+  # string representation
+  def __str__(self):
+    return str(self._value)
+
+class Note:
+  _value = 0
+
+  def __init__(self, value):
+    assert value >= 0
+    assert value <= 127
+
+    self._value = value
+
+  # comparisons
+  def __lt__(self, other):
+    return self._value < other._value
+
+  def __le__(self, other):
+    return self._value <= other._value
+
+  def __eq__(self, other):
+    return self._value == other._value
+
+  def __eq__(self, other):
+    return self._value != other._value
+
+  def __gt__(self, other):
+    return self._value > other._value
+
+  def __ge__(self, other):
+    return self._value >= other._value
+
+  # operations
+  def __add__(self, other):
+    self._value += other._value
+
+  def __sub__(self, other):
+    return Interval(self._value - other._value)
+
+
+  # string representation
+  def __str__(self):
+    return str(self._value) # TODO user-readable
+
+class Voice:
+  _notes = []
+  _range = (Note(48), Note(80))
+
+  def __init__(self, length):
+    # we initialise the list to None's
+    self._notes = [None] * length
+
+  # container type methods
+  def __getitem__(self, key):
+    return _notes[key]
+
+  def __setitem__(self, key, note):
+    assert self._range[0] <= note
+    assert note <= self._range[1]
+
+    self._notes[key] = note
+
+  # add a note to the voice
+  def add(self, note):
+    assert self._range[0] <= note
+    assert note <= self._range[1]
+
+    self._notes.append(note)
+
+  # string representation
+  def __str__(self):
+    output = "Voice\n"
+    output += "  range: " + self._range[0].__str__() + " to " + self._range[1].__str__() + "\n"
+
+    output += "  notes: "
+    for note in self._notes:
+      output += note.__str__() + " "
+
+    return output
+
+v = Voice(8)
+v[3] = Note(60)
+print v
+
+print Note(80) - Note(74)
