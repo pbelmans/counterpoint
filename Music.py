c427528e800c038baf13e32ea85ca7d7d38e318d
Pieter Belmans <pieterbelmans@gmail.com>
2014-12-07 10:53:49 +0100

    Fixed missing comparison operators for Interval
    No wonder RangeAtMostTenth was so messed up...

--- Music.py
+++ Music.py
@@ -15,6 +15,18 @@ class Interval:
 
   def __eq__(self, other):
     return self._value == other._value
+  
+  def __gt__(self, other):
+    return self._value > other._value
+  
+  def __ge__(self, other):
+    return self._value >= other._value
+  
+  def __lt__(self, other):
+    return self._value < other._value
+  
+  def __le__(self, other):
+    return self._value <= other._value
 
   # how big is the interval?
   def type(self):
